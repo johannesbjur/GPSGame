d1c0cff5138a210bca042c4f2293261ce650d38f
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2020-05-02 11:28:40 +0200

    project created

---
+++ GPSGame/app/src/main/java/com/example/gpsgame/MapFragment.kt
@@ -0,0 +1,43 @@
+package com.example.gpsgame
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import kotlinx.android.synthetic.main.fragment_map.*
+
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var googleMap: GoogleMap
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        mapView.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        map?.let {
+
+            googleMap = it
+        }
+    }
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+}
