f5bc136a09afec0b50c3c214cbd98af849af0563
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2020-05-12 13:46:05 +0200

    work on anonymous authentication started

--- GPSGame/app/src/main/java/com/example/gpsgame/MainActivity.kt
+++ GPSGame/app/src/main/java/com/example/gpsgame/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.gpsgame
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.navigationgame.PlaceItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
+import kotlin.collections.HashMap
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     var placeItems = mutableListOf<PlaceItem>()
 
     var db = FirebaseFirestore.getInstance()
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +35,14 @@ class MainActivity : AppCompatActivity() {
             R.id.navigation_map, R.id.navigation_list, R.id.navigation_profile ))
         navView.setupWithNavController(navController)
 
+        auth = FirebaseAuth.getInstance()
 
         val item = PlaceItem( "Gamla stan",59.325695, 18.071869 )
         val item2 = PlaceItem( "Tantolunden", 59.312975, 18.049348 )
         placeItems.add(item)
         placeItems.add(item2)
 
+        Log.d("date", item.created.toString())
 //
 //        val user: MutableMap<String, Any> = HashMap()
 //        user["first"] = "Ada"
@@ -53,4 +60,72 @@ class MainActivity : AppCompatActivity() {
 //            .addOnFailureListener { e -> Log.w("DB Callback: ", "Error adding document", e) }
 
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+
+
+        auth.signInAnonymously()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("Userlogin", "signInAnonymously:success")
+
+                    val user = auth.currentUser
+
+                    val data: MutableMap<String, Any> = HashMap()
+
+//                    Set user data
+//                    move somewhere else
+//                    data["first"] = "Johannes"
+//                    data["last"] = "Bjurstromer"
+//                    db.collection("users")
+//                        .document(auth.currentUser?.uid.toString())
+//                        .set(data)
+
+                    var docRef = db.collection("users")
+                                    .document(auth.currentUser?.uid.toString())
+                                    .collection("active")
+
+                    docRef.get().addOnSuccessListener { result  ->
+
+                        var firstActive = result.documents[0]
+
+                        Log.d( "activeDoc", firstActive.data?.get("created").toString())
+
+                        Log.d( "date", Date().time.toString())
+
+                        var compareDate = Date( firstActive.data?.get("created").toString() )
+                        var nowDate = Date()
+
+                        if ( compareDate.time < ( nowDate.time - 86400000 ) ) {
+
+                            Log.d( "activeDoc", "compare")
+//                            Clear active and create new active place items
+                        }
+
+                    }
+
+//                    val item = PlaceItem( "Gamla stan",59.325695, 18.071869 )
+//                    db.collection("users")
+//                        .document(auth.currentUser?.uid.toString())
+//                        .collection("active")
+//                        .add(item)
+
+
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("Userlogin", "signInAnonymously:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+
+
 }
--- GPSGame/app/src/main/java/com/example/gpsgame/PlaceItem.kt
+++ GPSGame/app/src/main/java/com/example/gpsgame/PlaceItem.kt
@@ -1,11 +1,14 @@
 package com.example.navigationgame
 
 import com.google.android.gms.maps.model.Circle
+import java.util.*
+
 
 class PlaceItem(val name: String, val latitude: Double, val longitude: Double, val radius: Double = 100.0 ) {
 
     val points: Int
     var circle: Circle? = null
+    var created = Date()
 
     init {
 
