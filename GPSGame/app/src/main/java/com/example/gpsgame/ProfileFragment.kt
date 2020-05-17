350258b77a4841379535b523187ee2d171435290
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2020-05-17 12:23:36 +0200

    work on profile ui

--- GPSGame/app/src/main/java/com/example/gpsgame/ProfileFragment.kt
+++ GPSGame/app/src/main/java/com/example/gpsgame/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.gpsgame
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,9 +8,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_profile.view.*
+import java.util.*
 
 
 class ProfileFragment : Fragment() {
@@ -30,12 +33,13 @@ class ProfileFragment : Fragment() {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        var docRef = db.collection("users")
+        val docRef = db.collection("users")
             .document(auth.currentUser?.uid.toString())
             .collection("placeItems")
 
@@ -47,6 +51,43 @@ class ProfileFragment : Fragment() {
 //            TODO Add medal calc value
             viewOfLayout.medal_value.text = "3"
         }
+
+
+        val date = Date(Date().time - 86400000 * 7)
+
+        Log.d("profilef", date.toString())
+
+        docRef.whereGreaterThanOrEqualTo("created", date).get().addOnSuccessListener { result ->
+
+            Log.d("profilef", result.documents.size.toString())
+
+            if ( result.documents.size > 0 ) {
+
+                val total = result.documents.size.toFloat()
+
+                var completed = 0.0F
+
+                for (document in result.documents) {
+
+                    if (document["completed"] as Boolean) completed++
+                }
+
+                var percentComplete = (completed / total * 100).toInt()
+
+                viewOfLayout.progress_value_text.text = percentComplete.toString() + "%"
+
+                if ( percentComplete == 0) percentComplete = 1
+                viewOfLayout.circle_progress_bar.setProgress(percentComplete, true)
+
+                Log.d("profilef", "${total} ${completed}")
+                Log.d("profilef", "${completed / total * 100}")
+            }
+            else {
+                viewOfLayout.progress_value_text.text = "0%"
+                viewOfLayout.circle_progress_bar.progress = 1
+            }
+        }
+
 
         viewOfLayout = inflater.inflate(R.layout.fragment_profile, container, false)
         viewOfLayout.user_full_name.text = activity.user_full_name
